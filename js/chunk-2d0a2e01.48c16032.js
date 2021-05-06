42efd950f7f205824d03a5b22eae024f6fa4f194
FrankieQi <377562448@qq.com>
2021-05-06 22:00:19 +0800

    first

---
+++ js/chunk-2d0a2e01.48c16032.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a2e01"],{"007b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"video-container"},[e._v("视频")])},c=[],i={name:"VideoIndex"},s=i,o=t("2877"),r=Object(o["a"])(s,a,c,!1,null,"133606a0",null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0a2e01.48c16032.js.map
